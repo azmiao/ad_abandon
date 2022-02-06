8492ec8baadc6ead80dc6ed3d79d977451b62587
azmiao <2362020227@qq.com>
2022-02-06 19:36:49 +0800

    ‘偷偷push’

---
+++ README.md
@@ -0,0 +1,21 @@
+# 啊这
+
+我不知道这是什么，一切仅为研究学习使用
+
+## 支持的网站
+
+[bimibimi](https://www.bimiacg4.net/)
+
+[去看吧](https://k8dm.com/)
+
+[假的樱花动漫](http://www.imomoe.live/)
+
+[age动漫](https://www.agemys.com/)
+
+[233动漫](https://www.dm233.cc/)
+
+## 干嘛的？
+
+看看代码？没几行
+
+或者你打开油猴插件，导入脚本`main.js`就知道了
---
+++ main.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         部分动漫网站去广告
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @icon         https://cdn.jsdelivr.net/gh/azmiao/picture-bed/img/%E5%9B%BE%E6%A0%87.png
+// @description  支持的网站：bimibimi，去看吧（即k8dm这个网站非常全，力荐），假的樱花动漫（真的樱花好像没有广告的），age动漫（最近好像炸了），233动漫
+// @author       鹿友のKICO
+// @match        *://*.bimiacg4.net/*
+// @match        *://k8dm.com/*
+// @match        *://*.imomoe.live/*
+// @match        *://*.agemys.com/*
+// @match        *://www.dm233.cc/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    //bimi和去看吧
+    setInterval(function () {
+        $("#HMRichBox").remove();
+        $("#HMcoupletDivleft").remove();
+        $("#HMcoupletDivright").remove();
+        //233动漫
+        $("#coupletright").remove();
+        $("#coupletleft").remove();
+    }, 500);
+    $("#fix_bottom_dom").remove();
+    //俩樱花
+    $("#HMcoupletDivleft").remove();
+    $("#HMcoupletDivright").remove();
+    $("#HMRichBox").remove();
+    $("#HMcoupletDivleft").remove();
+    $("#HMCOVER_ID1").remove();
+    //Age动漫
+    $('a[href*="https://vip.huijujiavip.com:8443?f=www.agemys.com"]').remove();
+})();
